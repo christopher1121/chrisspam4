1d25bdd85e8d5bdfb592b6720cdca56036ce52fb
christopher1121 <45854622+christopher1121@users.noreply.github.com>
2019-10-21 20:52:57 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By iRealKsA");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("574968739506814987")
+setInterval(function() {
+channel.send(`برعـاية فهودتشي الجميل غسيل اموال`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
